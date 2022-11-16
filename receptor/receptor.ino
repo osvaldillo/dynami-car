823aad36ebd3b56299d93f33f79b98cafe248276
Osvaldo Alferez <osvaldoalferez@gmail.com>
2022-11-15 19:56:41 -0600

    Version 1.0 de la prueba sin remoto

--- receptor/receptor.ino
+++ receptor/receptor.ino
@@ -58,75 +58,21 @@ void setup() {
 void loop() { //Inicia El blucle infinito "Lo que se encuentre en ese bucle se repetira infinitamente"
   analogWrite(enA, pwm);
   analogWrite(enB, pwm);
-  /*if (!radio.available()){ //Si radio no esta disponible 
-    digitalWrite(LEDconectividad, LOW); // apagar led
-  }
-  else{                          //si no
-      digitalWrite(LEDconectividad, HIGH); //encender led
-  }*/
   Serial.println(radio.available());
+  
   while (radio.available()){   //Si radio esta disponible 
+  
     radio.read(&Estado, sizeof(Estado)); //leer datos y almacenarlos en estado
     Serial.println(Estado); //imprimir en el monitor serial la variable EStado
+    
     switch(Estado){
       case 0: retroceder();
       case 1: avanzar();
       case 2: giroIzquierda();
       case 3: giroDerecha();
-    }
-    /*if (Estado == 1) {       //Sí, ingresa el número 1
-      digitalWrite(LEDrojo, LOW); // se manda un pulso bajo "LOW" "Led Apagado"
-      
-    }
-    
-    else  if (Estado == 0) { //Sí, ingresa el número 0
-      digitalWrite(LEDrojo, LOW); // se manda un pulso bajo "LOW" "Led Apagado"
-      
-    }
-    
-    else if (Estado == 3) { //Sí, ingresa el número 3 
-      digitalWrite(LEDazul, LOW); // se manda un pulso bajo "LOW" "Led Apagado"
-      digitalWrite(LEDrojo, LOW); // se manda un pulso bajo "LOW" "Led Apagado"
-      
-    }
-    
-    else  if (Estado == 2) { //Sí, ingresa el número 2 
-      digitalWrite(LEDazul, HIGH); // se manda un pulso alto "HIGH" "Led Encendido"
-      digitalWrite(LEDrojo, LOW); // se manda un pulso bajo "LOW" "Led Apagado"
-      
-      }*/
+    } 
   }
   detenerse();
-  /*
-  //Alto
-  else if (Estado == 4) {  //Sí, ingresa el número 4 
-  digitalWrite(MotordelanteroA,LOW);  // se manda un pulso bajo "LOW" al MotordelanteroA "Apagado"
-  }
-  //Izquierda
-  else  if (Estado == 5) { //Sí, ingresa el número 5 
-  digitalWrite(MotordelanteroA,HIGH); // se manda un pulso bajo "LOW" al MotordelanteroA "Encendido"
-  digitalWrite(MotordelanteroB,LOW);  // se manda un pulso alto "HIGH" al MotordelanteroB "Apagado"
-  }
-  //Alto
-  else if (Estado == 6) { //Sí, ingresa el número 6 
-  digitalWrite(MotordelanteroB,LOW); // se manda un pulso bajo "HIGH" al MotordelanteroB "Apagado"
-
-  }
-  //Derecha
-  else  if (Estado == 7) { //Sí, ingresa el número 7 
-  digitalWrite(MotordelanteroA,LOW);  // se manda un pulso bajo "LOW" al MotordelanteroA "Apagado"
-  digitalWrite(MotordelanteroB,HIGH); // se manda un pulso alto "HIGH" al MotordelanteroB "Encendido"
-  }
-  //Giro de 360° no se si funcione checar
-  else  if (Estado == 9) { //Sí, ingresa el número 9 
-  digitalWrite(MotordelanteroA,LOW);  // se manda un pulso bajo "LOW" al MotordelanteroA "Apagado"
-  digitalWrite(MotordelanteroB,HIGH); // se manda un pulso alto "HIGH" al MotordelanteroB "Encendido"
-  digitalWrite(LEDazul, HIGH);        // se manda un pulso alto "HIGH" "Led Encendido"
-  digitalWrite(MotordeposteriorA,HIGH); // se manda un pulso alto "HIGH" al MotordeposteriorA "Encendido"
-  digitalWrite(MotordeposteriorB,LOW); //se manda un pulso bajo "LOW" al MotordeposteriorB "Apagado"
-  delay(5000); 
-  }
-  */
 }
 
 void avanzar(){
